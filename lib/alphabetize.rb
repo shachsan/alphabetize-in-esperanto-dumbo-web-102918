9f495dcc39008c567326c7fe15d0e4c2239cc5b9
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 15:25:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,5 @@
 
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
- new_arr = arr.sort_by{|word|word.chars.map{|ch|esperanto.index(ch)}}
-
+  arr.sort_by{|word|word.chars.map{|ch|esperanto.index(ch)}}
 end
