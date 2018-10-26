a11b4da0f7ca05ddaa9edf4c78babe467f181724
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 15:14:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  # arr.each do |word|
+  #   word.splitarr.sort_by {|w|w.downcase}
+p  arr.split.chars.map{|ch|esperanto.index(ch)}
 end
