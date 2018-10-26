5e1bff70b4e6539286cfc35ad5c8c96333f3be8a
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 15:21:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ def alphabetize(arr)
   # code here
   # arr.each do |word|
   #   word.splitarr.sort_by {|w|w.downcase}
-new_arr = arr.each{|word|word.chars.map{|ch|esperanto.index(ch)}}
+p new_arr = arr.each{|word|word.chars.map{|ch|esperanto.index(ch)}}
 
 end
 alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
